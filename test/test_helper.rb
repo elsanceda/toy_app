bac63763919e12ab0892bce4dc5a3cb403e026d4
igsanceda <igsanceda@up.edu.ph>
2023-07-28 22:39:05 +0800

    Implement sessions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,4 +13,14 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   include ApplicationHelper
+
+  # Returns true if a test user is logged in.
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end  
 end
